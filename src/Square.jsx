f06d7212697c741221b207324d03623eaa2e3b05
Kaushik Saha <116095138+Kaushik1766@users.noreply.github.com>
2024-02-11 19:23:46 +0530

    removed hover effect

--- src/Square.jsx
+++ src/Square.jsx
@@ -4,7 +4,7 @@ function Square({ id, val, updateVal }) {
     const [cont, updateCont] = useState('X')
 
     return (
-        <div className='border hover:opacity-100 hover:scale-[1.05] bg-black select-none border-white h-[10vh] w-[10vh] flex flex-col justify-center ' onClick={() => { updateVal(id) }} id={id}>
+        <div className='border hover:opacity-75 bg-black select-none border-white h-[10vh] w-[10vh] flex flex-col justify-center ' onClick={() => { updateVal(id) }} id={id}>
             <p className='text-center  text-white text-4xl h-max' >
                 {val}
             </p>
